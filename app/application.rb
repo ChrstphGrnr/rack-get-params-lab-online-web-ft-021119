99df2a3e52b4933675bbd26ebb5b33655c1238f1
Christoph Gruener <christophgruener@me.com>
2019-03-27 21:52:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,8 +24,9 @@ class Application
       item = req.params["item"]
       if @@items.include?(item)
          @@cart << item
+         resp.write "added #{item}"
       else
-        resp.write "Path Not Found"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
